1ca6daf0cfaf04797e3114277cdec54d18ffbea8
DipuTony <dsingh197@gmail.com>
2021-10-03 21:02:53 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
+const name = "Dipu";
+
 ReactDOM.render(
   <h1>Hello, React!</h1>,
   document.getElementById('root')
 );
 
 ReactDOM.render(
-  <h1>Quanta Infotech!</h1>,
+  <h1>My Name is {name}.</h1>,
   document.getElementById('root1')
 );
