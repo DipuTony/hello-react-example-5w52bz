067639f3f0ab26948db76c5f8f7eb1f6628baf31
DipuTony <dsingh197@gmail.com>
2021-10-03 23:32:45 +0530

    2 files modified

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ ReactDOM.render(
   el,
   document.getElementById('root1')
 );
+
+let counter = 0;
+function show() {
+  counter++;
+  const el = <p>{counter}</p>;
+  ReactDOM.render(el, document.getElementById('timer'));
+}
+
+
+setInterval(show, 1000);
