cd0a509e6aa033660544cd5f347cf209eb489ddd
DipuTony <dsingh197@gmail.com>
2021-10-03 20:53:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello-react-example-5w52bz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hello-react-example-5w52bz)
---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+ReactDOM.render(
+  <h1>Hello, React!</h1>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <h1>Quanta Infotech!</h1>,
+  document.getElementById('root1')
+);
